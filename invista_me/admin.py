faedb269246e1448b4ed167320a5a38e7439dced
Pedro Renato Medeiros <medeirospedrorenato@gmail.com>
2023-09-09 18:54:54 -0300

    commit inicial

---
+++ invista_me/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Investimento
+
+
+admin.site.register(Investimento)
+
+# Register your models here.
---
+++ usuarios/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+
+def novo_usuario(request):
+    #tipo, validar, informar, salvar
+    if request.method == 'POST':
+        formulario = UserCreationForm(request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            usuario = formulario.cleaned_data.get('username')
+            messages.success(request,f'O usuario {usuario} foi criado com sucesso')
+            return redirect('login')
+    else:
+        formulario = UserCreationForm()
+        
+    return render(request, 'usuarios/registrar.html',{'formulario': formulario} )
